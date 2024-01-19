b129a1baf833229674689a918cebe5f581f8464c
sahil kumar <sahilkumarhp20@gmail.com>
2024-01-19 12:48:01 +0530

    Setting Up ENV in backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from 'express'
 const app = express();
+import dotenv from 'dotenv'
 
-const PORT = 3000;
+dotenv.config({path : 'backend/config/config.env'});
+
+const PORT = process.env.BACKEND_PORT;
 
 app.listen(PORT, () =>{
     console.log(`Server Start Successfully on ${PORT}`)
