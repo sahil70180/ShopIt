96fd94614f567b2559b3440fb3a2e5d6b553e75f
sahil kumar <sahilkumarhp20@gmail.com>
2024-01-21 20:42:42 +0530

    Fetching all products from database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,8 +2,17 @@ import Product from "../models/product.js";
 
 // Getting all Product ==> /api/v1/getproducts
 export const getProdcuts = async (req, res) =>{
+
+    const products = await Product.find();
+
+    if(!products){
+        res.status(400).json({
+            message: "No Product Found"
+        })
+    }
     res.status(200).json({
-        message : "All Products Fetched Successully"
+        message : "All Products Fetched Successully",
+        products,
     });
 };
 
@@ -13,7 +22,7 @@ export const newProduct = async (req, res) =>{
     const product = await Product.create(req.body);
 
     res.status(200).json({
-        message: "Prdocut Created Succesfully",
+        message: "Prodcut Created Succesfully",
         product,
     })
 };
