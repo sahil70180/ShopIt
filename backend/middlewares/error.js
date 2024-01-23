72a51efacf39159f03fe075b9e10ef65dbc625e9
sahil kumar <sahilkumarhp20@gmail.com>
2024-01-23 17:14:10 +0530

    Seperate the Development and Production Errors

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -4,7 +4,18 @@ export default (err, req, res, next) =>{
         message : err?.message || "Interval Server Error",
     };
 
-    res.status(error.statusCode).json({
-        message : error.message,
-    })
+    if(process.env.NODE_ENV === "DEVELOPMENT"){
+        res.status(error.statusCode).json({
+            message : error.message,
+            error : err,
+            stack : err?.stack,
+        })
+    }
+
+    if(process.env.NODE_ENV === "PRODUCTION"){
+        res.status(error.statusCode).json({
+            message : error.message,
+        })
+    }
+
 }
