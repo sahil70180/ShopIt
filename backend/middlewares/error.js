8b025d309bea73cf6449cd3ec285a4c36297e133
sahil kumar <sahilkumarhp20@gmail.com>
2024-01-23 17:55:04 +0530

    Handle invalid mongoose ID error and Validation Errors

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,20 +1,34 @@
-export default (err, req, res, next) =>{
-    let error = {
-        statusCode : err?.statusCode || 500,
-        message : err?.message || "Interval Server Error",
-    };
+import ErrorHandler from "../utils/errorHandler.js";
 
-    if(process.env.NODE_ENV === "DEVELOPMENT"){
-        res.status(error.statusCode).json({
-            message : error.message,
-            error : err,
-            stack : err?.stack,
-        })
-    }
+export default (err, req, res, next) => {
+  let error = {
+    statusCode: err?.statusCode || 500,
+    message: err?.message || "Interval Server Error",
+  };
 
-    if(process.env.NODE_ENV === "PRODUCTION"){
-        res.status(error.statusCode).json({
-            message : error.message,
-        })
-    }
-}
+  // handle invalid MongoDB ID Error
+  if (err.name == "CastError") {
+    const message = `Resource Not Found. Invalid : ${err?.path}`;
+    error = new ErrorHandler(message, 404);
+  }
+
+  // handle validations Error
+  if (err.name == "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 404);
+  }
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(error.statusCode).json({
+      message: error.message,
+      error: err,
+      stack: err?.stack,
+    });
+  }
+
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+};
