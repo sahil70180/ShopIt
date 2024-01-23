0c31e0b5c900ef1822a58d86b4ecb338df67d3ca
sahil kumar <sahilkumarhp20@gmail.com>
2024-01-23 12:36:52 +0530

    Creating seeder file and insert all Products

---
+++ backend/seeder/seeder.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import products from "./data.js";
+import Product from "../models/product.js"
+
+const seedProdcuts = async () =>{
+    try {
+
+        await mongoose.connect("mongodb://localhost:27017/ShOPIT");
+        
+        await Product.deleteMany();
+        console.log("Products are deleted");
+
+        await Product.insertMany(products);
+        console.log("Products are Added");
+
+        process.exit();
+        
+    } catch (error) {
+        console.log(error.message);
+        process.exit();
+    }
+};
+
+seedProdcuts();
