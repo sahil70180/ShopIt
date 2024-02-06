65d749e7a17c3d312e402964a5a4238d1289f02c
SAHIL KUMAR <sahilkumarhp20@gmail.com>
2024-02-06 12:25:17 +0530

    Fetching Order Details using order ID and Orders of LoggedIn user

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/order.js";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import Errorhandler from "../utils/errorHandler.js"
 
 //create new order ==> /api/v/orders/new
 export const newOrder = catchAsyncErrors(async (req, res, next) => {
@@ -31,3 +32,34 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   })
 });
+
+// get order details ==> /api/v1/orders/:id
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) =>{
+  const order = await Order.findById(req.params.id);
+  if(!order){
+    return next(new Errorhandler(`Not Order Found with this id : ${req.params.id}`, 404))
+  }
+
+  res.status(200).json({
+    message :"Order Details Found",
+    order,
+  })
+})
+
+//get all orders of current/loggedIN user ==> /api/v1/me/orders
+export const myOrders = catchAsyncErrors(async (req, res, next) =>{
+  const order = await Order.find({ user : req.user._id}).populate("user", "name email");
+  if(!order){
+    return next(new Errorhandler("No Orders Found", 404));
+  }
+
+  // if you recieve empyt array it means you did'nt order anything
+  if(order.length == 0){
+    return next(new Errorhandler("You have no orders till now", 404))
+  }
+
+  res.status(200).json({
+    message : "Your Order Details Fetched Successfully",
+    order,
+  })
+})
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { newOrder } from "../controllers/orderController.js";
+import { getOrderDetails, myOrders, newOrder } from "../controllers/orderController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.route("/orders/new").post(isAuthenticated, newOrder);
+router.route("/orders/:id").get(isAuthenticated, getOrderDetails);
+router.route("/me/orders").get(isAuthenticated, myOrders);
 
 export default router;
