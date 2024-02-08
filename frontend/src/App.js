d0b0496aba62e5ddd107de308d54c254413f28f0
SAHIL KUMAR <sahilkumarhp20@gmail.com>
2024-02-08 23:14:11 +0530

    Implement Routing and Server Home page using Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
-import "./App.css"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <div className="container">
-      <Home/>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
+    </BrowserRouter>
   );
 }
 
